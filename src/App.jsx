93130618cc1cfecfffe0259fb4decd3374facc0c
wided <widedkhadraoui1@gmail.com>
2024-10-14 00:01:43 +0200

    2eme commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,54 @@
-
-import React, { useState } from "react";
-import Sidebar from "./components/SidebarAdmin/AdminSidebar";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import LoginRegister from "./components/LoginRegister/LoginRegister";
 import MainContent from "./components/Main/Main";
 
-
 const App = () => {
-  const [darkMode,setDarkMode] = useState(true);
+    const [darkMode, setDarkMode] = useState(true);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [isOpen, setIsOpen] = useState(true);
 
-  const [isOpen, setIsOpen]=useState(true);
+    const toggleDarkMode = () => {
+        setDarkMode(!darkMode);
+    };
 
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  };
+    const toggleSidebar = () => {
+        setIsOpen(!isOpen);
+    };
 
-  const toggleSidebar = () => {
-    setIsOpen(!isOpen);
-  };
-  return (
-    <div className={ 'flex font-Montserrat bg-slate-700 $ {darkMode && "dark"}'}>
-      <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar}
-      />
-      <MainContent isOpen={isOpen} darkMode={darkMode} toggleDarkMode= {toggleDarkMode} />
-    </div>
-  );
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            setIsAuthenticated(true);
+        }
+    }, []);
+
+    return (
+        <Router>
+            <div>
+                <Routes>
+                    <Route path="/login" element={<LoginRegister setIsAuthenticated={setIsAuthenticated} />} />
+                    <Route path="/register" element={<LoginRegister setIsAuthenticated={setIsAuthenticated} />} />
+                    <Route 
+                        path="/main" 
+                        element={
+                            isAuthenticated ? (
+                                <MainContent 
+                                    isOpen={isOpen} 
+                                    darkMode={darkMode} 
+                                    toggleDarkMode={toggleDarkMode}
+                                    toggleSidebar={toggleSidebar} //  
+                                />
+                            ) : (
+                                <Navigate to="/login" />
+                            )
+                        } 
+                    />
+                    <Route path="*" element={<Navigate to="/login" />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 };
 
 export default App;
